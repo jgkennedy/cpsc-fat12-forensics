e4e3af8a070c661e366cb205ec158c75801717ba
Jesse Kennedy <jgkenne@clemson.edu>
2015-12-03 16:29:32 -0500

    starting code

--- notjustcats.c
+++ notjustcats.c
@@ -4,8 +4,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define FLOPPYSIZE 1474560
 
 int main(int argc, char *argv[]){
+	int fdin, fdout;
+	char *file;
 
-	printf( "\n\nTEST\n\n");
+	if ((fdin = open(argv[1], O_RDONLY)) < 0) {
+		printf("Can't open %s for reading", argv[1]);
+		return 1;
+	}
+
+	file = mmap(0, FLOPPYSIZE, PROT_READ, MAP_PRIVATE | MAP_FILE, fdin, 0);
+	printf("first character in file: %c.", file[0]);
 }
