f30ce0ea04554cac698fce1490b3abbaf6285472
Jesse Kennedy <jgkenne@clemson.edu>
2015-12-03 19:17:19 -0500

    better filename parsing and more traversing

--- notjustcats.c
+++ notjustcats.c
@@ -14,28 +14,55 @@
 int fdin; //, fdout;
 char *file;
 
+unsigned long cluster2offset(unsigned short cluster) {
+	return (cluster + 31) * 512;
+}
+
 // entries are 32B
 // clusters are 512B (enough to hold 16 directory entries)
-void traverse_directory(unsigned short start) {
-	printf("traversing 0x%x\n", start);
-	for (unsigned short offset = start; offset < start + 512; offset += 32) {
+void traverse_directory(unsigned long start) {
+	printf("traversing 0x%lx\n", start);
+	for (unsigned long offset = start; offset <= start + 512; offset += 32) {
 		// No more valid entries in the table
 		if (file[offset] == 0)
 			return;
 
-		char filename[12]; // 8 bytes for filename, 3 for ext, 1 for \0
-		memcpy(filename, &file[offset], 11);
-		filename[11] = (char) NULL;
-
 		unsigned short cluster;
 		memcpy(&cluster, &file[offset+26], 2);
 
-		unsigned long size;
-		memcpy(&size, &file[offset+28], 4);
+		// Check if entry is a directory
+		if ((file[offset+11] >> 4) & 1) {
+			// Don't traverse dot directories
+			if (file[offset] != '.')
+				traverse_directory(cluster2offset(cluster));
+		} else {
+			int deleted = 0;
+			char filename[12]; // 8 bytes for filename, 3 for ext, 1 for \0
+			memset(filename, 0, sizeof filename);
 
-		printf("Filename: %s\n", filename);
-		printf("Cluster: %d\n", cluster);
-		printf("Size: %ld\n", size);
+			int position = 0;
+			for (int i = 0; i < 11; i++) {
+				if (i == 8) {
+					filename[position++] = '.';
+				}
+				if (file[offset+i] == '\xE5') {
+					deleted = 1;
+					filename[position++] = '_';
+				} else if (file[offset+i] != '\x20') {
+					filename[position++] = file[offset+i];
+				}
+
+			}
+
+			unsigned long size;
+			memcpy(&size, &file[offset+28], 4);
+
+			printf("\nFilename: %s\n", filename);
+			printf("Cluster: %hu\n", cluster);
+			printf("Size: %lu\n", size);
+			if (deleted)
+				printf("DELETED\n");
+		}
 	}
 }
 
